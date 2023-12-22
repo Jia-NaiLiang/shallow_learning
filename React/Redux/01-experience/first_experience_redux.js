d1a0edec20b1b185ed6ae01a05e62b57ae1cce43
wenzekai <332684470@qq.com>
2023-12-22 16:29:44 +0800

    feat: 01-experience

---
+++ React/Redux/01-experience/first_experience_redux.js
@@ -0,0 +1,27 @@
+import { createStore } from "redux";
+// 1.store
+const store = createStore(reducer);
+
+// 2.reducer (pure function)
+function reducer(state = { value: 0 }, action) {
+  // calculate new state
+  switch (action.type) {
+    case "counter/increment":
+      return { value: state.value + 1 };
+    case "counter/decrement":
+      return { value: state.value - 1 };
+    default:
+      return state;
+  }
+}
+
+console.log(store);
+console.log(store.getState());
+
+store.subscribe(() => console.log("Updated successfully."));
+
+store.dispatch({ type: "counter/increment" });
+console.log(store.getState());
+store.dispatch({ type: "counter/increment" });
+store.dispatch({ type: "counter/decrement" });
+console.log(store.getState());
--- React/Redux/README.md
+++ React/Redux/README.md
@@ -1,1 +1,5 @@
 # Redux
+
+whole global state is stored in a single store.
+
+![redux_basics](images/redux_basics.png)
